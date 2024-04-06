3fe16c4e1bf05fa4afc6c2ab20b28721565a50ef
omolo-j <omoloj135@gmail.com>
2024-04-06 09:28:05 +0300

    sending data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+function submitData(name, email) {
+    const userData = {
+      name: name,
+      email: email
+    };
+  
+    const configObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(userData)
+    };
+  
+    return fetch("http://localhost:3000/users", configObject)
+      .then(response => response.json())
+      .then(data => {
+        const id = data.id;
+        document.body.innerHTML += id;
+      })
+      .catch(error => {
+        document.body.innerHTML += error.message;
+      });
+  }
+  
+  submitData("John Doe", "john@example.com");
+  
